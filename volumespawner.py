83c971f04d3f8902bd9ed86ca48d73359f13c01c
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2015-11-05 22:44:56 +0100

    crafting VolumeSpawner

--- volumespawner.py
+++ volumespawner.py
@@ -1,40 +1,32 @@
 import pwd
+import os
 
-from dockerspawner import DockerSpawner
+from dockerspawner import SystemUserSpawner
 from textwrap import dedent
 from traitlets import (
     Integer,
     Unicode,
 )
 from tornado import gen
+import getpass
 
+class VolumeSpawner(SystemUserSpawner):
+    """ My modification of SystemUserSpawner. """
 
-class SystemUserSpawner(DockerSpawner):
+    container_image = Unicode("jupyter/volumeuser", config=True)
+    container_prefix = Unicode("volumespawner", config=True)
 
-    container_image = Unicode("jupyter/systemuser", config=True)
+    homedir = Unicode("/home/jupyter", config=True)
+    host_username = Unicode(getpass.getuser(), config=True)
 
     host_homedir_format_string = Unicode(
-        "/home/{username}",
+        u"/home/{host_username}/spawned_users/{username}",
         config=True,
         help=dedent(
             """
             Format string for the path to the user's home directory on the host.
-            The format string should include a `username` variable, which will
-            be formatted with the user's username.
-            """
-        )
-    )
-
-    user_id = Integer(-1,
-        help=dedent(
-            """
-            If system users are being used, then we need to know their user id
-            in order to mount the home directory.
-
-            User IDs are looked up in two ways:
-
-            1. stored in the state dict (authenticator can write here)
-            2. lookup via pwd
+            The format string should include `'`host_username` and `username`
+            variable, which will be formatted with the host and user's username.
             """
         )
     )
@@ -44,81 +36,35 @@ class SystemUserSpawner(DockerSpawner):
         """
         Path to the volume containing the user's home directory on the host.
         """
-        return self.host_homedir_format_string.format(username=self.user.name)
+        return self.host_homedir_format_string.format(
+                host_username=self.host_username,
+                username=self.user.name)
 
-    @property
-    def homedir(self):
-        """
-        Path to the user's home directory in the docker image.
-        """
-        return "/home/{username}".format(username=self.user.name)
-
-    @property
-    def volume_mount_points(self):
-        """
-        Volumes are declared in docker-py in two stages.  First, you declare
-        all the locations where you're going to mount volumes when you call
-        create_container.
-
-        Returns a list of all the values in self.volumes or
-        self.read_only_volumes.
-        """
-        mount_points = super(SystemUserSpawner, self).volume_mount_points
-        mount_points.append(self.homedir)
-        return mount_points
-
-    @property
-    def volume_binds(self):
-        """
-        The second half of declaring a volume with docker-py happens when you
-        actually call start().  The required format is a dict of dicts that
-        looks like:
-
-        {
-            host_location: {'bind': container_location, 'ro': True}
-        }
-        """
-        volumes = super(SystemUserSpawner, self).volume_binds
-        volumes[self.host_homedir] = {
-            'bind': self.homedir,
-            'ro': False
-        }
-        return volumes
-
-    def _env_default(self):
-        env = super(SystemUserSpawner, self)._env_default()
-        env.update(dict(
-            USER=self.user.name,
-            USER_ID=self.user_id,
-            HOME=self.homedir
-        ))
-        return env
-    
     def _user_id_default(self):
         """
-        Get user_id from pwd lookup by name
-        
+        Get user_id of the host from pwd lookup by name
+
         If the authenticator stores user_id in the user state dict,
         this will never be called, which is necessary if
         the system users are not on the Hub system (i.e. Hub itself is in a container).
         """
-        return pwd.getpwnam(self.user.name).pw_uid
-
-    def load_state(self, state):
-        super().load_state(state)
-        if 'user_id' in state:
-            self.user_id = state['user_id']
-
-    def get_state(self):
-        state = super().get_state()
-        if self.user_id >= 0:
-            state['user_id'] = self.user_id
-        return state
+        return pwd.getpwnam(self.host_username).pw_uid
 
     @gen.coroutine
     def start(self, image=None):
-        """start the single-user server in a docker container"""
-        yield super(SystemUserSpawner, self).start(
-            image=image,
-            extra_create_kwargs={'working_dir': self.homedir}
-        )
+        """
+        Start the single-user server in a docker container
+
+        Makes sure that homedir on host side exists.
+        """
+        if not os.path.exists(self.host_homedir):
+            self.log.warn(
+                    "Folder '%s' doesn't exist. Creating.",
+                    self.host_homedir)
+            os.makedirs(self.host_homedir)
+        else:
+            self.log.info(
+                    "Folder '%s' exists.",
+                    self.host_homedir)
+
+        yield super(VolumeSpawner, self).start(image=image)
